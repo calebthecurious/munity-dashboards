0a2269fb68431e93df16eba4c3c9754d214480c0
calebtc <calebthecurious@gmail.com>
2023-09-03 01:56:03 +0700

    4th commit - form components

--- components/modals/guide-modal.tsx
+++ components/modals/guide-modal.tsx
@@ -1,20 +1,75 @@
 'use client';
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { useGuideModal } from "@/hooks/use-guide-modal";
 import { Modal } from "@/components/ui/modal";
+import { 
+    Form, 
+    FormControl, 
+    FormField, 
+    FormItem, 
+    FormLabel,
+    FormMessage
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
+const formSchema = z.object({
+    name: z.string().min(1),
+});
 
 export const GuideModal = () => {
     const guideModal = useGuideModal();
 
-        return (
-            <Modal
-                title="Create a new guide"
-                description="Add a new skill to teach your community"
-                isOpen={guideModal.isOpen}
-                onClose={guideModal.onClose}
-                >
-                    Become a guide form
-                </Modal>
-        )
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+        // TODO: Create Gude
+    }
+    
+    return (
+        <Modal
+            title="Create a new guide"
+            description="Add a new skill to teach your community"
+            isOpen={guideModal.isOpen}
+            onClose={guideModal.onClose}
+        >
+            <div className="space-y-4 py-2 pb-4">
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Munity" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <div className="pt-6 space-x-2 flex-items-center justify-end w-full">
+                        <Button 
+                            variant="outline" 
+                            onClick={guideModal.onClose}>
+                                Cancel
+                        </Button>
+                        <Button type="submit">Continue</Button>
+                    </div>
+                    </form>
+                </Form>
+            </div>
+        </Modal>
+    );
     };
