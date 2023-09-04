e66dfc643573b5f49b86a1a4befdd91106af96b4
calebtc <calebthecurious@gmail.com>
2023-09-04 22:31:13 +0700

    5th commit - prisma, planetScale, MySQL setup

--- components/modals/guide-modal.tsx
+++ components/modals/guide-modal.tsx
@@ -3,6 +3,7 @@
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios"
 
 import { useGuideModal } from "@/hooks/use-guide-modal";
 import { Modal } from "@/components/ui/modal";
@@ -16,13 +17,18 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
 
 const formSchema = z.object({
     name: z.string().min(1),
+
 });
 
 export const GuideModal = () => {
     const guideModal = useGuideModal();
+
+    const [loading, setLoading] = useState(false);
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -32,8 +38,27 @@ export const GuideModal = () => {
     });
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
-        // TODO: Create Gude
+        try {
+            setLoading(true);
+
+            throw new Error("x");
+
+            const response = await axios.post('/api/guides', values);
+
+            if (response.status === 200) {
+                toast.success("Guide Created");
+            } else {
+                toast.error(response.data.message || "Something went wrong!");
+            }
+        } catch (error) {
+            if (error.response && error.response.data && error.response.data.message) {
+                toast.error(error.response.data.message);
+            } else {
+                toast.error("Something went wrong!");
+            }
+        } finally {
+            setLoading(false);
+        }
     }
     
     return (
@@ -46,30 +71,41 @@ export const GuideModal = () => {
             <div className="space-y-4 py-2 pb-4">
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)}>
-                    <FormField
-                        control={form.control}
-                        name="name"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Name</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Munity" {...field} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <div className="pt-6 space-x-2 flex-items-center justify-end w-full">
-                        <Button 
-                            variant="outline" 
-                            onClick={guideModal.onClose}>
-                                Cancel
-                        </Button>
-                        <Button type="submit">Continue</Button>
-                    </div>
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input 
+                                            disabled={loading}
+                                            placeholder="Munity" 
+                                            {...field} 
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        <div className="pt-6 space-x-2 flex-items-center justify-end w-full">
+                            <Button 
+                                disabled={loading}
+                                variant="outline" 
+                                onClick={guideModal.onClose}>
+                                    Cancel
+                            </Button>
+                            <Button 
+                                disabled={loading}
+                                type="submit"
+                            >
+                                Continue
+                            </Button>
+                        </div>
                     </form>
                 </Form>
             </div>
         </Modal>
     );
-    };
+};
